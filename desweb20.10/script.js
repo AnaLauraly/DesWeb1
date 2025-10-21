953ce42863c3455f4178cc467e6f620caa0bc103
Ana Laura <ana.lazuly.dsdd@gmail.com>
2025-10-20 21:43:36 -0300

    Add files via upload

---
+++ desweb20.10/script.js
@@ -0,0 +1,12 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = 'seu mouse esta sobre o texto';
+}
+
+function ocultar(){
+document.getElementById('texto').innerHTML = ''
+
+}
+
+function cor(){
+    document.getElementById()
+}
